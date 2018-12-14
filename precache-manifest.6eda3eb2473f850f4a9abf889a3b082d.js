25b29058e51f02e260688f37658f9aff261065a9
Felipe Taboada <fliptaboada@gmail.com>
2018-12-14 19:02:03 -0200

    Updates

---
+++ precache-manifest.6eda3eb2473f850f4a9abf889a3b082d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5936e11c13ae64bda92658745d56085b",
+    "url": "/static/media/logo-thechurch.5936e11c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "639646f4964f0921afae",
+    "url": "/static/js/main.639646f4.chunk.js"
+  },
+  {
+    "revision": "a2f9d8c39ee856e47f94",
+    "url": "/static/js/1.a2f9d8c3.chunk.js"
+  },
+  {
+    "revision": "639646f4964f0921afae",
+    "url": "/static/css/main.ed0bdf14.chunk.css"
+  },
+  {
+    "revision": "a2f9d8c39ee856e47f94",
+    "url": "/static/css/1.7226cc62.chunk.css"
+  },
+  {
+    "revision": "afcd9b5bfe0c01267f6369032a842997",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/the-church/precache-manifest.f0e5fdffbe71377cc5cc2a6bb7e23e50.js"
+  "/precache-manifest.6eda3eb2473f850f4a9abf889a3b082d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/the-church/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
