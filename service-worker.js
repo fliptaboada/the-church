141ea9ae310bc8c8aa876d06fb77dd51cab63556
Felipe Taboada <fliptaboada@gmail.com>
2019-03-04 18:30:35 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9daf556d57a292388c6b2a2c3392be3.js"
+  "/precache-manifest.c546526969cade6c6f66eb22df8a32b8.js"
 );
 
 workbox.clientsClaim();
